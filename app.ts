cb48964435a6bc435a3ecafa9161ef1cd3bb291e
Ala Eddine <ala.official.acc@gmail.com>
2023-09-10 20:30:30 +0100

    :tada: Solve isObjectEmpty problem

--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
 import { removeDuplicates } from "./src/remove-duplications/";
+import { isEmpty } from "./src/javascript/easy/isObjectEmpty";
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+console.log(`is ${[1, 2, 3, 4]} empty:`, isEmpty([1, 2, 3, 4]));
+console.log(
+  `is ${{ name: "Ala Eddine", age: 30 }} empty:`,
+  isEmpty({ name: "Ala Eddine", age: 30 }),
+);
